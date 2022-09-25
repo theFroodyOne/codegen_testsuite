a00eb227dba500ddfe5085923d38304d05ec4663
theFroodyOne <97949736+theFroodyOne@users.noreply.github.com>
2022-09-25 19:20:21 +0200

    Adding more test cases

--- testCodeGen.sh
+++ testCodeGen.sh
@@ -2,10 +2,11 @@
 
 javac TestCodeGen.java
 export JASMIN_JAR=../src/jasmin.jar
-for i in {001..047}
+for i in {001..061}
 do
 	../bin/alanc ./codegen_testcases/codegen$i.alan > compilation.out
 	java codegen$i > ./codegen_testcases/actual_output/codegen$i.out
 	java TestCodeGen ./codegen_testcases/desired_output/codegen$i.out ./codegen_testcases/actual_output/codegen$i.out $i
 done
 rm compilation.out
+echo "Done!"
